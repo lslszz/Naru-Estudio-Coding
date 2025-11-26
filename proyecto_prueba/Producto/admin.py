c06ad46c07ed5f92e11a2ff35ac101675f1b391a
ediuh <edie2566@gmail.com>
2025-11-26 14:32:46 -0300

    feat: Automatiza views panel admin

--- proyecto_prueba/Producto/admin.py
+++ proyecto_prueba/Producto/admin.py
@@ -1,19 +1,39 @@
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.db import models
+from .models import Lista
 
-# Modelo ficticio (solo para que aparezca el bloque en el admin)
-class ProductoFicticio(models.Model):
+
+modelsViews = []
+
+class AdminModel_HomeProducto(models.Model):
+    url = '/productos/'
     class Meta:
-        verbose_name = "Productos"
-        verbose_name_plural = "Productos"
-        managed = False  # No crea ninguna tabla en la base de datos
+        verbose_name = "View Home Producto"
+        verbose_name_plural = "View Home Producto"
+        managed = False
 
-# Clase del admin: solo redirige al hacer clic
-class ProductoAdminLink(admin.ModelAdmin):
-    def changelist_view(self, request, extra_context=None):
-        # Redirige al template real de productos
-        return redirect('/productos/')  # URL que ya funciona
+class AdminModel_LeerProductos(models.Model):
+    url = '/productos/leer-todos'
+    class Meta:
+        verbose_name = "View Leer Producto"
+        verbose_name_plural = "View Leer Producto"
+        managed = False
 
-# Registrar el modelo en el admin
-admin.site.register(ProductoFicticio, ProductoAdminLink)
+modelsViews.append(AdminModel_HomeProducto)
+modelsViews.append(AdminModel_LeerProductos)
+
+
+# REGISTRO DE VIEWS
+def Registrar_Views():
+    for model in modelsViews:
+        class AdminLink(admin.ModelAdmin):
+            url = model.url
+            def changelist_view(self, request, extra_context=None):
+                # Redirige al template real de productos
+                return redirect(self.url)  # URL que ya funciona
+        admin.site.register(model, AdminLink)
+
+Registrar_Views()
+
+admin.site.register(Lista)
