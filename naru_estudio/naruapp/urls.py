9eee822f8f2b5411abc05fd3bdf7e977ceb13eb1
ediuh <edie2566@gmail.com>
2025-12-02 19:06:51 -0300

    feat: Agrega Read completo de entradas y añade favicon

--- naru_estudio/naruapp/urls.py
+++ naru_estudio/naruapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('registro-exitoso/', views.cuenta_registro_ok, name="cuenta_registro_ok"),
     path('clases/', views.naru_clases),
     path('entradas/', views.naru_entradas),
+    path('entradas/<int:id>', views.naru_entrada, name="naru_entrada"),
+    path('entradas/crear-entrada', views.naru_entrada_nueva, name="naru_entrada_nueva")
     #path('crear/', views.crear_producto, name='crear_producto'),
     #path('leer/<int:id>', views.leer_producto, name='leer_producto'),
     #path('leer-todos', views.leer_todos, name='leer_todos'),
