a1292a1139e3e584f01cd1daead2055fb3679e5d
ediuh <edie2566@gmail.com>
2025-12-04 11:58:55 -0300

    feat: Agrega icono de clases y mejora desplegable estático

--- naru_estudio/naruapp/static/naruapp/js/script.js
+++ naru_estudio/naruapp/static/naruapp/js/script.js
@@ -5,16 +5,23 @@ window.onload = function() {
         window.location.href = "/naru-estudio/entradas";
     };
 
+    function LinkeaClases() {
+        window.location.href = "/naru-estudio/clases";
+    };
+
     function OcultarPilar () {
         pilar.classList.toggle("hidden");
     }
 
     // MANEJADOR DE EVENTOS
     const divExplorar = document.querySelector(".divExplorar")
+    const divClases = document.querySelector(".divClases")
     const divHamburguesa = document.querySelector(".divHamburguesa")
     const pilar = document.querySelector(".sidebar")
 
     divExplorar.addEventListener("click", LinkeaExplorar)
+    divClases.addEventListener("click", LinkeaClases)
     divHamburguesa.addEventListener("click", OcultarPilar)
+
 
 }
